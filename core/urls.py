d3ae829e2c87217b1f804ee39524159ea5d991fb
JuansitxxDEV <108484017+Juansitxx@users.noreply.github.com>
2026-03-12 07:26:24 +0000

    changelog v6. cambiar urls.py desde el core

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("tienda.urls", namespace="tienda")),
+    path("", include("academia.urls", namespace="academia")),
 ]
